39982deadb9710cbfc4337f9f44cc2c932250959
Koki Arakawa <k.arkw87@gmail.com>
2023-02-26 22:22:14 +0900

    add No 102

---
+++ 102. Binary Tree Level Order Traversal/myAns.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = []
+        ans = []
+
+        if root:
+            queue.append(root)
+        
+        while queue:
+            size = len(queue)
+            level = []
+
+            for i in range(size):
+                node = queue.pop(0)
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans.append(level)
+        
+        return ans
