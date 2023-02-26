7cb0977e6edfdb12bd33802d3cfb0d92e17572b3
Koki Arakawa <k.arkw87@gmail.com>
2023-02-26 12:39:20 +0900

    add 142

---
+++ 142. Linked List Cycle II/myAns.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while head != slow:
+                    head = head.next
+                    slow = slow.next
+                return head
+        
+        return None
